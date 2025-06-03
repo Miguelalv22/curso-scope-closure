e8438feb60ef8019f2198301de9061265b210dfd
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-06-03 10:57:50 -0600

    Global Scope

---
+++ src/scope/global.js
@@ -0,0 +1,14 @@
+const fruit = 'apple';  //declaracion y asignacion
+function bestFruit() {
+    console.log(`la mejor fruta es ${fruit}`);
+}
+console.log(fruit)
+
+function countries() {
+    country = 'Colombia';//asignacion variable Global
+    console.log(country);
+}
+console.log(country)
+
+bestFruit();
+countries();
