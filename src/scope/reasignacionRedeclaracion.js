5808792edc8c8f9ecf28194dc6d9256e5f680b9f
Miguel Lopez <miguel.2211.lopez@gmail.com>
2025-06-03 12:05:35 -0600

    Reasignacion y Redeclaracion

---
+++ src/scope/reasignacionRedeclaracion.js
@@ -0,0 +1,19 @@
+var firstName;   //Undefined
+firstName = 'Oscar';
+
+var lastName = 'David'; // declarar / asignar
+lastName = 'Ana'; //reasignar
+
+var secondName = 'David'; //declarando / asignando
+var secondName = 'Ana'; //reasignado
+
+//Let
+let fruit = 'Apple'; //declara y asigna
+fruit = 'Kiwi'; //reasignar
+
+//let fruit = 'Mango' //Cannot redeclared block-scoped variable 'fruit
+
+//Const
+const animal = 'dog';//declara y asigna
+//animal = 'cat'; //asignar //TypeError: Assignment to constant variable
+//const animal = 'Snake' //Cannot redeclared block-scoped variable 'fruit
